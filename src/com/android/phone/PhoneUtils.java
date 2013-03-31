ebb01fb646e39f388fe87c71c7b5512f6fb2cee8
DvTonder <david.vantonder@gmail.com>
2013-03-31 12:10:01 -0400

    Phone: Add Blacklist functionality
    Original code written for CM7 by Cytown, Cyanogen and nhnt11.
    
    Modified for CM10.1 by Danny Baumann and dvtonder.
    Drawables by Björn Lundén
    
    Change-Id: Ib6b8ed4b3410d8553ca061086ba7bcf3b059b1d0
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/call_feature_setting.xml
    	src/com/android/phone/CallFeaturesSetting.java
    	src/com/android/phone/CallNotifier.java
    	src/com/android/phone/PhoneGlobals.java
    	src/com/android/phone/PhoneUtils.java

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Blacklist - Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +25,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -361,6 +363,47 @@ public class PhoneUtils {
         if (DBG) log("==> hungup = " + hungup);
 
         return hungup;
+    }
+
+    static Call getCurrentCall(Phone phone) {
+        Call ringing = phone.getRingingCall();
+        Call fg = phone.getForegroundCall();
+        Call bg = phone.getBackgroundCall();
+        if (!ringing.isIdle()) {
+            return ringing;
+        }
+        if (!fg.isIdle()) {
+            return fg;
+        }
+        if (!bg.isIdle()) {
+            return bg;
+        }
+        return fg;
+    }
+
+    static Connection getConnection(Phone phone, Call call) {
+        if (call == null) {
+            return null;
+        }
+        if (phone.getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
+            return call.getLatestConnection();
+        }
+        return call.getEarliestConnection();
+    }
+
+    static class PhoneSettings {
+        static boolean isBlacklistEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_enable_blacklist", false);
+        }
+        static boolean isBlacklistNotifyEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_nofify", false);
+        }
+        static boolean isBlacklistRegexEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_blacklist_regex", false);
+        }
+        private static SharedPreferences getPrefs(Context context) {
+            return PreferenceManager.getDefaultSharedPreferences(context);
+        }
     }
 
     static boolean hangupRingingCall(Call ringing) {
@@ -1595,7 +1638,11 @@ public class PhoneUtils {
             // return it to the user.
 
             cit = new CallerInfoToken();
-            cit.currentInfo = (CallerInfo) userDataObject;
+            if (userDataObject instanceof String) { // only blacklist will cause this, so just ignore this.
+                cit.currentInfo = new CallerInfo();
+            } else {
+                cit.currentInfo = (CallerInfo) userDataObject;
+            }
             cit.asyncQuery = null;
             cit.isFinal = true;
             // since the query is already done, call the listener.
